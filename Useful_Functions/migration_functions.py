9ea8c7ec4776fcec9b39edf817ba3f24bf04febe
Lea Yamashiro <71639931+lea-yamashiro@users.noreply.github.com>
2024-02-02 22:54:27 -0800

    Update migration_functions.py

--- Useful_Functions/migration_functions.py
+++ Useful_Functions/migration_functions.py
@@ -28,3 +28,33 @@ def migration_organizing(data1):
     mig_table = mig_table[mig_table['date'] > 2013] 
 
     return mig_table
+
+''' This function is a manual way of creating a dataframe that is usually performed by 
+    a "group_by" function. The reason for this distinction is because we used the group_by
+    function, but were really struggling to perform table-operations on the group_by frame, 
+    as that function creates a special type of dataframe. This method was much faster.'''
+
+def setup_long(dataframe):
+    
+    #percentiles = {'values': [50, 75, 80, 90]}
+    unique_country_indices = dataframe.index.unique()
+
+    if not unique_country_indices.empty:
+        
+        # Create an empty DataFrame with columns
+        results = pd.DataFrame(columns=['Net Migration', 'Migration Rate (%)'])
+
+        for country_index in unique_country_indices:
+            country_data = dataframe.loc[country_index]
+            mig_net_avg = country_data['Net Migration'].mean()
+            mig_percap_avg = country_data['Migration Rate (%)'].mean()
+
+            # Append the computed averages to the results DataFrame
+            results.loc[country_index] = [mig_net_avg, mig_percap_avg]
+        
+        # calculate the percentiles for the migration rates 
+        results['Percentile Rank'] = results['Migration Rate (%)'].apply(
+            lambda x: stats.percentileofscore(results['Migration Rate (%)'], x))
+
+        return results 
+    
